42999d1ede456f558827ace0558924e75bc08b8a
natehill22 <natehill22@gmail.com>
2025-10-03 16:07:02 -0700

    Uploading Project1 Expressions Alert
    and JS file

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+var Q = "I say, \"Where is His Majesty\'s String Cheese!\""; //Defining Q as an escaped string
+
+window.alert(Q) //Calling the Q variable in an alert pop up window
+
+var B = ("\"Better butter beats bitter butter battered " //Defining a variable and giving it a concatenated bunch of text
+    + "because better butter\'s not bitter like bitter butter "
+    + "\'n battered butter at best makes butter better.\"");
+
+//document.write(B); //Calling the B variable
+
+var FamilyName = "Whale", Beluga = "Belinda", Right = "Ronny", Gray = "Gary", Blue = "Blaire", Sperm = "We don't talk about him"; //Defining multiple variables that belong to the Whale fmaily
+
+//document.write(Gray); //Calling the specified whale's name (gray)
+
+var A = 11;
+
+document.write(31); //Calling a literal expression
+document.write("<br>"); //Adding a line break
+document.write(8 * 2); //Calling an arithmetic expression
+document.write("<br>"); //Adding a line break
+document.write(A); //Calling a variable reference 
+
+var Sent1 = "This is the beginning of the string"; //Defining the Sent1 variable as a string
+var Sent2 = " and this is the end of the string"; //Defining the Sent2 variable as a string
+document.write("<br>"); //Adding a line break
+document.write(Sent1 + Sent2); //Calling and concatenating the Sent1 and Sent2 variables into one string
